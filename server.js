8e6de3e9f924b70a616739a50513d86247db00b3
Rakesh Chourasia <141241896+Rakeshchourasia@users.noreply.github.com>
2025-09-22 12:32:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 
 const allowedOrigins = [
 "https://land-portal-1.onrender.com", // your frontend
-  "http://localhost:3000",  
+  "http://localhost:5173",  
 ];
 
 app.use(
